ee24a8d74a29fd7231bc4c9b02efd37766e867fe
princejoyel-dotcom <princejoyel2005@gmail.com>
2025-09-30 13:00:10 +0800

    Update reels.dart

--- facebook/lib/pages/reels.dart
+++ facebook/lib/pages/reels.dart
@@ -10,6 +10,6 @@ class ReelsPage extends StatefulWidget {
 class _ReelsPageState extends State<ReelsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: Column());
   }
 }
